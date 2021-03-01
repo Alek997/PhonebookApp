594eea2e73b330bdf1d83c920030f8980151fc0b
alek.temimovic <alek.temimovic@netcast.rs>
2021-03-01 18:53:40 +0100

    fixed bug with long state name

--- src/screens/ContactScreen.tsx
+++ src/screens/ContactScreen.tsx
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
   property: {
     display: 'flex',
     flexDirection: 'row',
-    marginLeft: 20,
+    marginHorizontal: 20,
     alignItems: 'center'
   }
 })
